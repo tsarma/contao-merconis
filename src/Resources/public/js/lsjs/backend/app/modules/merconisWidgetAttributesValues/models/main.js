61e1104aa27df2f67df5ab91292177b3c7299aa1
Volker Stuber <stuber@leadingsystems.de>
2018-01-18 12:49:19 +0100

    add templates and basic template/data handling

--- src/Resources/public/js/lsjs/backend/app/modules/merconisWidgetAttributesValues/models/main.js
+++ src/Resources/public/js/lsjs/backend/app/modules/merconisWidgetAttributesValues/models/main.js
@@ -2,8 +2,8 @@ var obj_classdef_model = {
 	name: 'main',
 	
 	data: {
-		json_value: '[]',
-		arr_value: []
+		json_data: '[]',
+		arr_data: []
 	},
 
 	obj_dataFunctionBindings: {
@@ -11,8 +11,8 @@ var obj_classdef_model = {
 		 * The data object holds the value in JSON format and in an array.
 		 * The following data function bindings make sure that each of them gets updated when the other is changed.
 		 */
-		'json_value': 'saveJsonValue',
-		'arr_value': 'saveArrValue'
+		'json_data': 'saveJsonData',
+		'arr_data': 'saveArrData'
 	},
 
 	start: function() {
@@ -23,17 +23,19 @@ var obj_classdef_model = {
 		this.__module.onModelLoaded();
 
 		this.loadOriginalJsonData();
+
+		this.__view.initializeGui();
 	},
 
 	loadOriginalJsonData: function() {
-		this.writeData('json_value', this.__view.el_originalInputField.getProperty('value'));
+		this.writeData('json_data', this.__view.el_originalInputField.getProperty('value'));
 	},
 
-	saveJsonValue: function(var_newData, str_registeredDataPath, var_originalData, str_actualDataPath) {
-		this.writeData('arr_value', JSON.parse(var_newData), false, false);
+	saveJsonData: function(var_newData, str_registeredDataPath, var_originalData, str_actualDataPath) {
+		this.writeData('arr_data', JSON.parse(var_newData), false, false);
 	},
 
-	saveArrValue: function(var_newData, str_registeredDataPath, var_originalData, str_actualDataPath) {
-		this.writeData('json_value', JSON.stringify(var_newData), false, false);
+	saveArrData: function(var_newData, str_registeredDataPath, var_originalData, str_actualDataPath) {
+		this.writeData('json_data', JSON.stringify(var_newData), false, false);
 	}
 };
--- src/Resources/public/js/lsjs/backend/app/modules/merconisWidgetAttributesValues/view.js
+++ src/Resources/public/js/lsjs/backend/app/modules/merconisWidgetAttributesValues/view.js
@@ -6,19 +6,37 @@ var str_moduleName = '__moduleName__';
 
 var obj_classdef = 	{
 	el_originalInputField: null,
+	el_originalLabel: null,
+	str_transferInputFieldName: '',
+	str_label: '',
 
 	start: function() {
-		this.initializeGui();
-	},
-
-	initializeGui: function() {
-		this.__el_container.setStyle('background-color', '#FFAA00');
-		console.log(this.__el_container);
-
 		this.el_originalInputField = this.__el_container.getElement('input');
 		if (typeOf(this.el_originalInputField) !== 'element') {
 			console.error('original input field not found.')
 		}
+
+		this.str_transferInputFieldName = this.el_originalInputField.getProperty('name');
+
+		this.el_originalLabel = this.__el_container.getElement('label');
+		if (typeOf(this.el_originalLabel) === 'element') {
+			this.str_label = this.el_originalLabel.getProperty('html');
+		}
+	},
+
+	initializeGui: function() {
+		this.tplReplace({
+			name: 'main'
+		});
+
+		this.updateAttributeValueAssignmentFields();
+	},
+
+	updateAttributeValueAssignmentFields: function() {
+		this.tplReplace({
+			name: 'attributeValueAssignment',
+			parent: this.__autoElements.main.attributeValueAssignment
+		});
 	}
 };
 
