251ee2109493be209166727999153e0bba29256e
Volker Stuber <stuber@leadingsystems.de>
2018-01-21 02:31:56 +0100

    optimize and finish the lsjs module "merconisWidgetAttributesValues"

--- src/Resources/public/js/lsjs/backend/app/modules/merconisWidgetAttributesValues/view.js
+++ src/Resources/public/js/lsjs/backend/app/modules/merconisWidgetAttributesValues/view.js
@@ -6,9 +6,6 @@ var str_moduleName = '__moduleName__';
 
 var obj_classdef = 	{
 	el_originalInputField: null,
-	el_originalLabel: null,
-	str_transferInputFieldName: '',
-	str_label: '',
 
 	start: function() {
 		this.el_originalInputField = this.__el_container.getElement('input');
@@ -16,21 +13,16 @@ var obj_classdef = 	{
 			console.error('original input field not found.')
 		}
 
-		this.str_transferInputFieldName = this.el_originalInputField.getProperty('name');
-
-		this.el_originalLabel = this.__el_container.getElement('label');
-		if (typeOf(this.el_originalLabel) === 'element') {
-			this.str_label = this.el_originalLabel.getProperty('html');
-		}
-
 		this.__models.main.loadOriginalJsonData();
 		this.initializeGui();
 	},
 
 	initializeGui: function() {
-		this.tplReplace({
+		var el_mainGui = this.tplPure({
 			name: 'main'
 		});
+
+		el_mainGui.replaces(this.el_originalInputField);
 
 		this.__autoElements.main.btn_addRow.addEvent(
 			'click',
@@ -58,6 +50,9 @@ var obj_classdef = 	{
 	},
 
 	initializeFields: function() {
+		if (typeOf(this.__autoElements.attributeValueAssignment.assignmentInput) !== 'elements') {
+			return;
+		}
 		this.__autoElements.attributeValueAssignment.assignmentInput.addEvent(
 			'change',
 			this.updateStoredData.bind(this)
