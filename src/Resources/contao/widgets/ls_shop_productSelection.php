1e183a7f6a1b695ed506c375a8e77fcada69b92a
David Molineus <david.molineus@netzmacht.de>
2018-02-19 17:11:20 +0100

    Load custom routing.yml and use router to generate the link.

--- src/Resources/contao/widgets/ls_shop_productSelection.php
+++ src/Resources/contao/widgets/ls_shop_productSelection.php
@@ -29,7 +29,7 @@ class ls_shop_productSelection extends \Widget
 		?>
 		<div class="widgetProductSelection">
 			<input type="hidden" id="<?php echo $this->strId; ?>" name="<?php echo $this->strName; ?>" value="<?php echo $this->varValue; ?>" />
-			<a onclick="ls_shop_productSelection.setCurrentlyActiveWidgetElement(getParent()); Backend.getScrollOffset();Backend.openModalIframe({'width':765,'title':'<?php echo specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['be_productSearch']['text008'])); ?>','url':this.href,'id':'<?php echo $this->strId; ?>'});return false;" href="vendor/leadingsystems/contao-merconis/src/Resources/contao/pub/ls_shop_beProductSearch.php?mode=productSelection" title="<?php echo specialchars($GLOBALS['TL_LANG']['be_productSearch']['text008']); ?>">
+			<a onclick="ls_shop_productSelection.setCurrentlyActiveWidgetElement(getParent()); Backend.getScrollOffset();Backend.openModalIframe({'width':765,'title':'<?php echo specialchars(str_replace("'", "\\'", $GLOBALS['TL_LANG']['be_productSearch']['text008'])); ?>','url':this.href,'id':'<?php echo $this->strId; ?>'});return false;" href="<?= \System::getContainer()->get('router')->generate('merconis.backend.product_search', ['mode' => 'productSelection']) ?>" title="<?php echo specialchars($GLOBALS['TL_LANG']['be_productSearch']['text008']); ?>">
 				<?php echo \Image::getHtml('filemanager.gif', $GLOBALS['TL_LANG']['MSC']['fileManager'], 'style="vertical-align:text-bottom"'); ?>
 			</a>
 			<div class="selectedProductOutput">
