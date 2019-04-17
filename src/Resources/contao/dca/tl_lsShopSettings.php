51c79f0042a96582edaebe71cc5026174788d64c
vstuber <stuber@leadingsystems.de>
2019-04-17 10:14:05 +0200

    Closes #47

--- src/Resources/contao/dca/tl_lsShopSettings.php
+++ src/Resources/contao/dca/tl_lsShopSettings.php
@@ -15,7 +15,7 @@ $GLOBALS['TL_DCA']['tl_lsShopSettings'] = array(
 	'palettes' => array(
 		'__selector__' => array('ls_shop_useVATIDValidation'),
 		'default' => '
-		{basic_legend},ls_shop_serial,ls_shop_country,ls_shop_currency,ls_shop_currencyCode,ls_shop_numDecimals,ls_shop_priceRoundingFactor,ls_shop_priceType,ls_shop_numDecimalsWeight,ls_shop_weightUnit,ls_shop_quantityDefault,ls_shop_versandkostenType,ls_shop_autoSelectCheapestPossibleShippingAndPaymentMethods,ls_shop_ownEmailAddress,ls_shop_output_definitionset,ls_shop_delivery_infoSet,ls_shop_productDetailsTemplate,ls_shop_standardProductImageFolder,ls_shop_standardProductImageDelimiter,ls_shop_imageSortingStandardDirection,ls_shop_beOrderTemplateOverview,ls_shop_beOrderTemplateDetails;
+		{basic_legend},ls_shop_serial,ls_shop_country,ls_shop_currency,ls_shop_currencyCode,ls_shop_numDecimals,ls_shop_priceRoundingFactor,ls_shop_priceType,ls_shop_numDecimalsWeight,ls_shop_weightUnit,ls_shop_quantityDefault,ls_shop_versandkostenType,ls_shop_autoSelectCheapestPossibleShippingAndPaymentMethods,ls_shop_useProductDescriptionAsSeoDescription,ls_shop_ownEmailAddress,ls_shop_output_definitionset,ls_shop_delivery_infoSet,ls_shop_productDetailsTemplate,ls_shop_standardProductImageFolder,ls_shop_standardProductImageDelimiter,ls_shop_imageSortingStandardDirection,ls_shop_beOrderTemplateOverview,ls_shop_beOrderTemplateDetails;
 		
 		{euSettings_legend},ls_shop_ownVATID,ls_shop_euCountrycodes,ls_shop_useVATIDValidation;
 
@@ -540,6 +540,13 @@ $GLOBALS['TL_DCA']['tl_lsShopSettings'] = array(
 			'eval'                    => array('tl_class'=>'w50 m12')
 		),
 		
+		'ls_shop_useProductDescriptionAsSeoDescription' => array(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_lsShopSettings']['ls_shop_useProductDescriptionAsSeoDescription'],
+			'exclude'                 => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('tl_class'=>'w50 m12')
+		),
+
 		'ls_shop_useVATIDValidation' => array(
 			'label'                   => &$GLOBALS['TL_LANG']['tl_lsShopSettings']['ls_shop_useVATIDValidation'],
 			'exclude'                 => true,
--- src/Resources/contao/frontendModules/ModuleProductSingleview.php
+++ src/Resources/contao/frontendModules/ModuleProductSingleview.php
@@ -63,6 +63,13 @@ class ModuleProductSingleview extends \Module {
 		// Overwrite the page title
 		$objPage->pageTitle = strip_insert_tags($objProduct->_title).' - '.($objPage->pageTitle ? $objPage->pageTitle : $objPage->title);
 
+		if (
+            isset($GLOBALS['TL_CONFIG']['ls_shop_useProductDescriptionAsSeoDescription'])
+            &&	$GLOBALS['TL_CONFIG']['ls_shop_useProductDescriptionAsSeoDescription']
+        ) {
+            $objPage->description = ($objProduct->_shortDescription || $objProduct->_description) ? substr(strip_insert_tags(strip_tags($objProduct->_shortDescription ? $objProduct->_shortDescription : $objProduct->_description)), 0, 350) : $objPage->description;
+        }
+
 		if ($objProduct->_hasKeywords != '') {
 			$GLOBALS['TL_KEYWORDS'] .= (strlen($GLOBALS['TL_KEYWORDS']) ? ', ' : '').$objProduct->_keywords;
 		}
